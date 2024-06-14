695d14c2ac9365a437b57c659acbda183f73e1eb
Ishaan Singh <ishaan.singh2k@gmail.com>
2024-06-15 00:02:32 +0530

    Adding validation using express-validator

--- my-notebook/backend/db.js
+++ my-notebook/backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/";
+const mongoURI = "mongodb://localhost:27017/my-notebook"; // my-notebook is database name
 
 const connectToMongo = async () =>{
     mongoose.connect(mongoURI).then(()=>console.log("connected to mongo")).catch((e)=>console.log(e.message))
--- my-notebook/backend/routes/auth.js
+++ my-notebook/backend/routes/auth.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const Users = require('../models/Users');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
 // Creating user using : POST "/api/auth"
-router.post('/', (req, res) => {
-    const user = Users(req.body)
-    console.log(req.body)
-    user.save()
-    res.send(req.body)
+router.post('/',[
+    body('name', 'Name is required').not().isEmpty(),
+    body('username', 'Please enter unique username').not().isEmpty(),
+    body('password', 'Please enter a password with 3 or more characters').isLength({ min: 3 }),
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+    Users.create({
+        name: req.body.name,
+        username: req.body.username,
+        password: req.body.password
+      }).then(user => res.json(user));
 })
 
 module.exports = router
