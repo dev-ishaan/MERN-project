6044c22daed00ca05fb901a73b5c15545ffc1bdf
Ishaan Singh <ishaan.singh2k@gmail.com>
2024-06-15 00:43:11 +0530

    Adding condition for same username available check

--- my-notebook/backend/index.js
+++ my-notebook/backend/index.js
@@ -4,7 +4,7 @@ connectToMongo()
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000 // 3000 is for react
 
 app.use(express.json())
 
--- my-notebook/backend/routes/auth.js
+++ my-notebook/backend/routes/auth.js
@@ -3,21 +3,38 @@ const Users = require('../models/Users');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 
-// Creating user using : POST "/api/auth"
-router.post('/',[
+// Creating user using : POST "/api/auth/createuser"
+router.post('/createuser',[
     body('name', 'Name is required').not().isEmpty(),
     body('username', 'Please enter unique username').not().isEmpty(),
     body('password', 'Please enter a password with 3 or more characters').isLength({ min: 3 }),
-], (req, res) => {
+], async (req, res) => {
+
+    // If errors exists, then this code is executed
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
       }
-    Users.create({
-        name: req.body.name,
-        username: req.body.username,
-        password: req.body.password
-      }).then(user => res.json(user));
+
+      // Check whether the user with same username exists?
+      try {
+        let user = await Users.findOne({ username: req.body.username });
+        if (user) {
+            return res.status(400).json({ errors: [{ msg: 'Username already exists' }]
+                })
+            }
+        // Creating a user
+        user = await Users.create({
+            name: req.body.name,
+            username: req.body.username,
+            password: req.body.password
+        })
+        res.json(user)
+      }
+      catch(error){
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+      }
 })
 
 module.exports = router
