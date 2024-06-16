4b6e09bf0989661aa42ef7262dff771e2c00bd26
Ishaan Singh <ishaan.singh2k@gmail.com>
2024-06-16 18:47:23 +0530

    Using bcrypt.js and JWT for authentication

--- my-notebook/backend/routes/auth.js
+++ my-notebook/backend/routes/auth.js
@@ -2,39 +2,58 @@ const express = require('express');
 const Users = require('../models/Users');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const JWTSecretKey = 'secretOrPrivateKey';
 
 // Creating user using : POST "/api/auth/createuser"
 router.post('/createuser',[
     body('name', 'Name is required').not().isEmpty(),
     body('username', 'Please enter unique username').not().isEmpty(),
     body('password', 'Please enter a password with 3 or more characters').isLength({ min: 3 }),
-], async (req, res) => {
+  ], async (req, res) => {
 
-    // If errors exists, then this code is executed
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-      }
+      // If errors exists, then this code is executed
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+          return res.status(400).json({ errors: errors.array() });
+        }
 
-      // Check whether the user with same username exists?
-      try {
-        let user = await Users.findOne({ username: req.body.username });
-        if (user) {
-            return res.status(400).json({ errors: [{ msg: 'Username already exists' }]
-                })
+        // Check whether the user with same username exists?
+        try {
+          let user = await Users.findOne({ username: req.body.username });
+          if (user) {
+              return res.status(400).json({ errors: [{ msg: 'Username already exists' }]
+                  })
+              }
+
+          const salt = await bcrypt.genSalt(10)
+          const secretPass = await bcrypt.hash(req.body.password, salt)
+
+          // Creating a user in db
+          user = await Users.create({
+              name: req.body.name,
+              username: req.body.username,
+              password: secretPass
+          })
+
+          // Using id of user to sign it into auth token
+          const data = {
+            user : {
+              id: user.id
             }
-        // Creating a user
-        user = await Users.create({
-            name: req.body.name,
-            username: req.body.username,
-            password: req.body.password
-        })
-        res.json(user)
-      }
-      catch(error){
-        console.error(error.message);
-        res.status(500).send("Internal Server Error");
-      }
+          }
+          // Adding JWTSecretKey to make it more secure
+          const authToken = jwt.sign(data, JWTSecretKey)
+
+          // res.json(user)
+          res.json({authToken})
+        }
+        catch(error){
+          console.error(error.message);
+          res.status(500).send("Internal Server Error");
+        }
 })
 
 module.exports = router
