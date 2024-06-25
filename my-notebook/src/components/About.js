bb6118f84e4a6cd0b66c49f11c00f375db9c0ce6
Ishaan Singh <ishaan.singh2k@gmail.com>
2024-06-25 23:03:12 +0530

    Using useContext hook and ContextAPI

--- my-notebook/src/components/About.js
+++ my-notebook/src/components/About.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import noteContext from '../context/notes/NoteContext'
 
 const About = () => {
+  const a = useContext(noteContext)
+  
+  useEffect(() => {
+    a.update()
+    // eslint-disable-next-line
+  }, []);
   return (
     <div>
-      About
+      This is about {a.state.name} and she is in class {a.state.class}
     </div>
   )
 }
---
+++ my-notebook/src/context/notes/NoteState.js
@@ -0,0 +1,27 @@
+import noteContext from './NoteContext'
+import {useState} from 'react'
+
+const NoteState = (props) =>{
+
+    const s1 = {
+        "name": "Harshita",
+        "class": "10 B"
+    }
+
+    const [state, setState] = useState(s1)
+    const update = () =>{
+        setTimeout(() => {
+            setState({
+                "name": "Ishaan",
+                "class": "12 B"
+            })
+        }, 1000);
+    }
+    return(
+        <noteContext.Provider value={{state, update}}>
+            {props.children}
+        </noteContext.Provider>
+    )
+}
+
+export default NoteState
