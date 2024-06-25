7c3e44d4d046b31f4a15791ff13db6435a2a5437
Ishaan Singh <ishaan.singh2k@gmail.com>
2024-06-25 23:52:54 +0530

    Using useLocation hook to change active buttons on navbar

--- my-notebook/src/components/About.js
+++ my-notebook/src/components/About.js
@@ -2,15 +2,18 @@ import React, { useContext, useEffect } from 'react'
 import noteContext from '../context/notes/NoteContext'
 
 const About = () => {
-  const a = useContext(noteContext)
   
-  useEffect(() => {
-    a.update()
-    // eslint-disable-next-line
-  }, []);
+  // const a = useContext(noteContext)
+  
+  // useEffect(() => {
+  //   a.update()
+  //   // eslint-disable-next-line
+  // }, []);
+
   return (
     <div>
-      This is about {a.state.name} and she is in class {a.state.class}
+      {/* This is about {a.state.name} and she is in class {a.state.class} */}
+      This is about page
     </div>
   )
 }
--- my-notebook/src/context/notes/NoteState.js
+++ my-notebook/src/context/notes/NoteState.js
@@ -3,22 +3,23 @@ import {useState} from 'react'
 
 const NoteState = (props) =>{
 
-    const s1 = {
-        "name": "Harshita",
-        "class": "10 B"
-    }
+    // const s1 = {
+    //     "name": "Harshita",
+    //     "class": "10 B"
+    // }
 
-    const [state, setState] = useState(s1)
-    const update = () =>{
-        setTimeout(() => {
-            setState({
-                "name": "Ishaan",
-                "class": "12 B"
-            })
-        }, 1000);
-    }
+    // const [state, setState] = useState(s1)
+    // const update = () =>{
+    //     setTimeout(() => {
+    //         setState({
+    //             "name": "Ishaan",
+    //             "class": "12 B"
+    //         })
+    //     }, 1000);
+    // }
     return(
-        <noteContext.Provider value={{state, update}}>
+        // <noteContext.Provider value={{state, update}}>
+        <noteContext.Provider value={{}}>
             {props.children}
         </noteContext.Provider>
     )
